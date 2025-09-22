3b6710139ee0dbfd025523963be25d55a1a463b9
Jaime de Venecia <jaime@augihealth.com>
2025-09-21 20:49:13 -0700

    refactor: swap .env for generic config file to accommodate browser

---
+++ vanilla-js-example/config.example.js
@@ -0,0 +1,5 @@
+export const config = {
+  SPOTIFY_CLIENT_ID: '<your spotify client ID here>',
+  SPOTIFY_CLIENT_SECRET: '<your spotify client secret here>',
+  SPOTIFY_REDIRECT_URI: '<your spotify redirect URI here>',
+}
