ddb2dd451db1ee3123e1c6184e3feca1d899ebd9
Jaime de Venecia <jaime@augihealth.com>
2025-09-23 13:09:06 -0700

    chore: delete deprecated vjs example

--- vanilla-js-example-old/config.example.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-  SPOTIFY_CLIENT_ID: '<your spotify client ID here>',
-  SPOTIFY_CLIENT_SECRET: '<your spotify client secret here>',
-  SPOTIFY_REDIRECT_URI: '<your spotify redirect URI here>',
-}
