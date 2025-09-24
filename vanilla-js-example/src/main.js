77e8e20b66e4426e668557cba4ea3ac282ebd958
Jaime de Venecia <jaime@augihealth.com>
2025-09-23 23:05:24 -0700

    refactor: disable prettier on main vjs file, annotate with pseudo-code, update UI

--- vanilla-js-example/src/main.js
+++ vanilla-js-example/src/main.js
@@ -1,113 +1,110 @@
-const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID
-const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET
-const redirect_uri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI
+// prettier-ignore-file
 
-const params = new URLSearchParams(window.location.search)
-const authorizationCode = params.get('code')
+// secure credentials imported from .env file
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET
+const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI
 
+// main function that initializes the authentication + authorization flow
 async function initializeApp() {
-  if (authorizationCode) {
-    const accessToken = await getAccessToken(
-      client_id,
-      client_secret,
-      redirect_uri,
-      authorizationCode
-    )
-    const profile = await fetchProfile(accessToken)
-    console.log(profile)
-    populateUI(profile)
+  const urlParams = new URLSearchParams(window.location.search)
+  const authorizationCode = urlParams.get('code')
+
+  if (authorizationCode === null) {
+    obtainAuthorizationCode(clientId, redirectUri)
   } else {
-    obtainAuthorizationCode(client_id, redirect_uri)
+    const accessToken = await getAccessToken(clientId, clientSecret, redirectUri, authorizationCode)
+
+    const profile = await fetchProfile(accessToken)
+    populateUI(profile)
   }
 }
 initializeApp()
 
+// step 1: obtain authorization code from spotify's authorization endpoint
 function obtainAuthorizationCode(clientId, redirectUri) {
-  // https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  const scope = 'user-top-read'
-
-  // https://stackoverflow.com/questions/26132066/what-is-the-purpose-of-the-state-parameter-in-oauth-authorization-request
+  // best practice for preventing CSRF attacks
   const state = generateRandomString(16)
 
   // create query string with appropriate parameters
-  const params = new URLSearchParams({
+  const urlParams = new URLSearchParams({
     response_type: 'code',
     client_id: clientId,
     redirect_uri: redirectUri,
-    scope,
-    state,
-
-    // for demo purposes; ensures that spotify's auth dialog is always shown
+    state: state,
     show_dialog: 'true',
+    // (^ for demo purposes; ensures that spotify's auth dialog is always shown)
   }).toString()
 
-  document.location = 'https://accounts.spotify.com/authorize?' + params
+  document.location = 'https://accounts.spotify.com/authorize?' + urlParams
 }
 
-async function getAccessToken(
-  clientId,
-  clientSecret,
-  redirectUri,
-  authorizationCode
-) {
+// step 2: exchange authorization code for access token
+async function getAccessToken(clientId, clientSecret, redirectUri, authorizationCode) {
   try {
-    // obtain access token with authorization code
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
+    // make 'POST' HTTP request to spotify's token endpoint
+    const response = await fetch(
+      'https://accounts.spotify.com/api/token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic ' + createBase64EncodedString(clientId, clientSecret)
+        },
+        body: new URLSearchParams({
+          code: authorizationCode,
+          redirect_uri: redirectUri,
+          grant_type: 'authorization_code',
+        })
+      }
+    )
+    const data = await response.json()
 
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization:
-          'Basic ' + createBase64EncodedString(clientId, clientSecret),
-      },
-
-      body: new URLSearchParams({
-        code: authorizationCode,
-        redirect_uri: redirectUri,
-        grant_type: 'authorization_code',
-      }),
-    })
-    if (!response.ok)
-      throw new Error('Failed to obtain access token from Spotify')
-
-    const { access_token } = await response.json()
-
-    return access_token
+    return data.access_token
   } catch (error) {
     console.error(error.message)
   }
 }
 
+// step 3: fetch user profile data from spotify's user profile endpoint
 async function fetchProfile(accessToken) {
-  const response = await fetch('https://api.spotify.com/v1/me', {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${accessToken}` },
-  })
+  const response = await fetch(
+    'https://api.spotify.com/v1/me',
+    {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }
+  )
   const data = await response.json()
 
   return data
 }
 
+// step 4: use DOM manipulation to update the UI with our profile data
 function populateUI(profile) {
-  document.getElementById('display-name').innerText = profile.display_name
-  document.getElementById('avatar').setAttribute('src', profile.images[0].url)
-  document.getElementById('user-id').innerText = profile.id
-  document.getElementById('profile-uri').innerText = profile.uri
-  document
-    .getElementById('profile-uri')
-    .setAttribute('href', profile.external_urls.spotify)
-  document.getElementById('profile-image').innerText = profile.images[0].url
-  document
-    .getElementById('profile-image')
-    .setAttribute('href', profile.images[0].url)
+  const displayNameElement = document.getElementById('display-name')
+  displayNameElement.innerText = profile.display_name
+
+  const avatarElement = document.getElementById('avatar')
+  avatarElement.setAttribute('src', profile.images[0].url)
+
+  const userIdElement = document.getElementById('user-id')
+  userIdElement.innerText = profile.id
+
+  const profileUriElement = document.getElementById('profile-uri')
+  profileUriElement.innerText = profile.uri
+  profileUriElement.setAttribute('href', profile.external_urls.spotify)
+
+  const profileImageLinkElement = document.getElementById('profile-image')
+  profileImageLinkElement.innerText = profile.images[0].url
+  profileImageLinkElement.setAttribute('href', profile.images[0].url)
 }
 
 /* helper functions */
 
 function generateRandomString(length) {
   let randomString = ''
-  const charSet =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
   for (let i = 0; i < length; i++) {
     randomString += charSet.charAt(Math.floor(Math.random() * charSet.length))
@@ -119,6 +116,7 @@ function generateRandomString(length) {
 function createBase64EncodedString(clientId, clientSecret) {
   const basicAuthString = clientId + ':' + clientSecret
 
+  // create a Base64-encoded ASCII string from a binary string
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/btoa
   const base64EncodedString = btoa(basicAuthString)
 
